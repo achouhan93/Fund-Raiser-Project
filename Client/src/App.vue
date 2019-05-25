7e27ee5cc17f1f74f244bdf9d722accceb80e87d
Paneesh <PPrabhur@its.jnj.com>
2019-05-25 13:01:42 +0200

    Seperating Client-side code and Server-side code into two seperate folders and adding Homepage loginpage and registration page in client

---
+++ Client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-app>
+      <page-header/>
+      <main>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import PageHeader from '@/components/Header.vue'
+export default {
+  name: 'App',
+  components: {
+    PageHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.v-btn__content {
+  color: white;
+}
+.v-toolbar__content {
+  color: white;
+}
+</style>
---
+++ Client/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-toolbar fixed class="cyan">
+        <v-toolbar-title class="mr-4"> Fund Raiser </v-toolbar-title>
+        <v-toolbar-items>
+            <v-btn flat dark> Browse Causes </v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+            <v-btn flat dark @click="navigateToRigester({name: 'register' })">
+                SignUp
+            </v-btn>
+            <v-btn flat dark>
+                Login
+            </v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateToRigester (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
